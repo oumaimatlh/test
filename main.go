632f50e161c0d559af06f5aa22fce8b3e7ae0e6c
oumaimatlh <159827758+oumaimatlh@users.noreply.github.com>
2025-11-25 23:25:10 +0100

    New Version

--- main.go
+++ main.go
@@ -2,91 +2,183 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func EditFile(content string) {
-	words := []string{}
-	word := ""
-	b := false
-	for _, r := range content {
-		if r != '\n' && r != '\t' && r != ' ' {
-			word += string(r)
-			b = true
-		} else if b {
-			words = append(words, word)
-			word = ""
-			b = false
+func EditedText(Text string) []string {
+	//Auto-Correction => Supprimer les espaces et les tabulation inutiles
+	//Diviser chaque suite de caracteres qui sont séprarer par Espace
+	//RQ => Dans ce cas j'ai laissé /n malgré est parmi les espaces blanc ; mais j'ai le besoin a la fin a fin de retourner le text avec ces sautes de lignes
+
+	Words := []string{}
+	Word := ""
+	Check := false
+
+	for _, r := range Text {
+		if r != '\t' && r != ' ' {
+			Word += string(r)
+			Check = true
+		} else if Check {
+			Words = append(Words, Word)
+			Word = ""
+			Check = false
 		}
 	}
-
-	if word != "" {
-		words = append(words, word)
+	if Word != "" {
+		Words = append(Words, Word)
 	}
 
-	//newText := ""
-	// Traitement word by word
-	//WordBefore := ""
-	for j := 0; j < len(words); j++ {
-		substring := "(up)"
-		k := len(substring)
-		match := ""
-		h := ""
-		for i := 0; i < len(words[j]); i++ {
-			if k <= len(words[j]) {
-				match = string(words[j][i:k])
-			} else {
-				match = string(words[j][i:])
+	//Editing :
+	//Search a Tags
+	Tags := []string{"(up)", "(low)", "(bin)", "(hex)", "(cap)"}
 
-			}
+	for i := 0; i < len(Words); i++ {
+		for j := 0; j < len(Tags); j++ {
+			Tag := Tags[j]
+			LenTag := len(Tag)
+			Matching := ""
 
-			if match == substring {
-				WordBefore := words[j][:i]
-				if len(WordBefore) == 0 {
-					WordBefore = words[j-1]
-					h = strings.ToUpper(WordBefore)
+			for q := 0; q < len(Words[i]); q++ {
 
+				if LenTag <= len(Words[i]) {
+					Matching = string(Words[i][q:LenTag])
 				} else {
-					h = strings.ToUpper(WordBefore)
+					Matching = string(Words[i][q:])
 				}
-				fmt.Println(h)
 
-			} 
-			k++
-		}
-	}
+				if Matching == Tag {
+					//chaque '(Tag)' va faire son travail selon  le type de ce tag => cases
 
-		for j := 0; j < len(words); j++ {
-		substring := "(low)"
-		k := len(substring)
-		match := ""
-		h := ""
-		for i := 0; i < len(words[j]); i++ {
-			if k <= len(words[j]) {
-				match = string(words[j][i:k])
-			} else {
-				match = string(words[j][i:])
+					WordBefore := Words[i][:q] // On va stocker le mot qui juste avant le tag sans espace
+					WordAfter := Words[i][q+len(Tag):]
 
-			}
+					index := i - 1
 
-			if match == substring {
-				WordBefore := words[j][:i]
-				if len(WordBefore) == 0 {
-					WordBefore = words[j-1]
-					h = strings.ToLower(WordBefore)
+					if len(WordBefore) == 0 {
+						if i > 0 {
+							WordBefore := Words[index]
 
+							for (WordBefore == "\n" || WordBefore == "") && index >= 0 {
+								index--
+								WordBefore = Words[index]
+							}
+							
+
+							switch Tag {
+							case "(up)":
+								Words[index] = strings.ToUpper(WordBefore)
+								Words[i] = WordAfter
+
+							case "(low)":
+								Words[index] = strings.ToLower(WordBefore)
+								Words[i] = WordAfter
+
+							case "(bin)":
+								t, _ := strconv.ParseInt(WordBefore, 2, 64)
+								Words[index] = strconv.FormatInt(t, 10)
+								Words[i] = WordAfter
+
+							case "(hex)":
+								t, _ := strconv.ParseInt(WordBefore, 16, 64)
+								Words[index] = strconv.FormatInt(t, 10)
+								Words[i] = WordAfter
+
+							case "(cap)":
+								Words[index] = strings.ToUpper(string(WordBefore[0])) + strings.ToLower(WordBefore[1:])
+								Words[i] = WordAfter
+
+							}
+						} else {
+							Words[i] = WordAfter
+						}
+
+					} else if WordBefore == "\n" {
+						if i > 0 {
+							WordBefore = Words[index]
+
+							for WordBefore == "\n" {
+								if i-1 <= 0 {
+									break
+								}
+								i--
+								WordBefore = Words[index]
+
+							}
+
+							switch Tag {
+							case "(up)":
+								Words[index] = strings.ToUpper(WordBefore)
+								Words[i] = "\n" + WordAfter
+
+							case "(low)":
+								Words[index] = strings.ToLower(WordBefore)
+								Words[i] = "\n" + WordAfter
+
+							case "(bin)":
+								t, _ := strconv.ParseInt(WordBefore, 2, 64)
+								Words[index] = strconv.FormatInt(t, 10)
+								Words[i] = "\n" + WordAfter
+
+							case "(hex)":
+								t, _ := strconv.ParseInt(WordBefore, 16, 64)
+								Words[index] = strconv.FormatInt(t, 10)
+								Words[i] = "\n" + WordAfter
+
+							case "(cap)":
+								if len(WordBefore) > 0 {
+									Words[index] = strings.ToUpper(string(WordBefore[0])) + strings.ToLower(WordBefore[1:])
+								}
+								Words[i] = "\n" + WordAfter
+
+							}
+						} else {
+							Words[i] = WordAfter
+						}
+
+					} else {
+						// Words[i][:q] est une suite de carac et non espace blanc et non  len(Words[i][:q]) == 0
+						switch Tag {
+						// dans ce cas le mot avant est tjr Words[i][:q]
+						case "(up)":
+							Words[i] = strings.ToUpper(WordBefore)+ WordAfter
+							fmt.Println(WordBefore)
+
+						case "(low)":
+							Words[i] = strings.ToLower(WordBefore) + WordAfter
+						case "(bin)":
+							t, _ := strconv.ParseInt(WordBefore, 2, 64)
+							Words[i] = strconv.FormatInt(t, 10) + Words[i][q+len(Tag):]
+
+						case "(hex)":
+							t, _ := strconv.ParseInt(WordBefore, 16, 64)
+							Words[i] = strconv.FormatInt(t, 10) +  Words[i][q+len(Tag):]
+
+						case "(cap)":
+							fmt.Println(WordBefore)
+							if len(WordBefore) > 0 {
+								fmt.Println(WordBefore)
+								fmt.Println(Words[i][q+len(Tag):])
+								Words[i] = strings.ToUpper(string(WordBefore[0])) + strings.ToLower(WordBefore[1:]) +  WordAfter
+							}
+
+						}
+						
+					
+					
+					}
+					q = -1
+					
+						
 				} else {
-					h = strings.ToLower(WordBefore)
+					LenTag++
 				}
-				fmt.Println(h)
-
-			} 
-			k++
+			}
 		}
 	}
-
+	return Words
 }
+
 func main() {
-	EditFile("it (cap) was the best of times, it was the worst of times    (up)) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS ILOVEYY      (low) winter of despair.\nSimply add 42 (hex) and 10 (bin) and you will see the result is 68.")
+	fmt.Println(EditedText("hello(low)world(up)"))
 }
- 
